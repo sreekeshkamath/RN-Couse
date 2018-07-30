82c1b956a017012e0b346116a87865e7efade887
Sreekesh Kamath <sreekeshkamath95@gmail.com>
2018-07-30 10:25:33 +0530

    Changed the Style of Text Input

--- rn-course-1/src/screens/Auth/Auth.js
+++ rn-course-1/src/screens/Auth/Auth.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startTabs from '../MainTabs/startMainTabs';
-
+import DefaultInput from "../../components/UI/DefaultInputStyles";
 class AuthScreen extends Component {
     loginHandler = () => {
         startTabs();
     }
     render() {
         return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler} />
+            <View style={styles.container}>
+                <Text>Please Log In</Text>
+                <Button title="Switch to Login" />
+                <View style={styles.inputContainer}>
+                    <DefaultInput style = {styles.input} placeholder="Your Email Address" />
+                    <DefaultInput placeholder="Password" />
+                    <DefaultInput placeholder="Confirm Password" />
+                </View>
+                <Button title="Submit" onPress={this.loginHandler} />
+
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center", // Vertical
+        flex: 1,
+        alignItems: "center",
+    },
+    inputContainer: {
+        width: "80%"
+    }, 
+    input: {
+        backgroundColor: "#ee3",
+    }
+});
 
 export default AuthScreen; 
